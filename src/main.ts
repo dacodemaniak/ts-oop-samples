c78f3be409f351967bcdc16fa69b70af75ef2b7c
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-10-28 15:24:32 +0200

    UPDATE: Add another instance of StagiaireModel

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { POEModel } from "./models/poe-model";
 import { StagiaireModel } from "./models/stagiaire-model";
 
 /**
@@ -9,7 +10,24 @@ export class Main {
     public constructor() {
         const stagiaire: StagiaireModel = new StagiaireModel();
         stagiaire.setBirthDate(new Date(2042, 3, 30));
+        stagiaire.lastName = 'Aubert';
+        stagiaire.firstName = 'Jean-Luc';
         console.log('Stagiaire : ' + stagiaire.getBirthDate());
+
+        const poe: POEModel = new POEModel();
+        poe.title = 'Fullstack Java Angular';
+        poe.setDates(new Date(2023, 0, 27), new Date(2022, 9, 24));
+
+        stagiaire.poe = poe;
+
+        const bond: StagiaireModel = new StagiaireModel();
+        bond.firstName = 'James';
+        bond.lastName = 'Bond';
+        bond.setBirthDate(new Date(1900, 0, 7));
+        bond.poe = poe;
+
+        console.log(stagiaire.toString());
+        console.log(bond.toString());
     }
 }
 
