1b0e1f5c1adf18cc1d31e7bae69a8812d1c15cf1
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-10-28 15:23:57 +0200

    UPDATE: toString method

--- src/models/stagiaire-model.ts
+++ src/models/stagiaire-model.ts
@@ -1,8 +1,10 @@
+import { POEModel } from "./poe-model";
+
 export class StagiaireModel {
     public firstName: string = '';
     public lastName: string = '';
     private birthDate: Date = new Date();
-    public poe: string = '';
+    public poe: POEModel = new POEModel();
 
     public setBirthDate(birthDate: Date): void {
         const today: Date = new Date();
@@ -15,5 +17,18 @@ export class StagiaireModel {
 
     public getBirthDate(): Date {
         return this.birthDate;
+    }
+
+    public toString(): string {
+        return `${this.firstName} ${this.lastName}
+            ${this.birthDate.getDate()}-${this.birthDate.getMonth() + 1}-${this.birthDate.getFullYear()} 
+            ${this.poe.toString()}
+        `;
+    }
+
+    public toString2(): string {
+        return this.firstName + ' ' + this.lastName + '\n' +
+            this.birthDate.getDate() + '/' + this.birthDate.getMonth() + 1 + '/' + this.birthDate.getFullYear() + '\n' +
+            this.poe.toString();
     }
 }
