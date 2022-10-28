625fc923fbd10ced29c5d06eb0420c5c3ee4cd00
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-10-28 10:23:04 +0200

    ADD: Main class with constructor - Main instanciation

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/**
+ * main.ts : Entry point in oop-ts application
+ * @author Aélion 2022-10
+ * @version 1.0.0
+ */
+export class Main {
+    public constructor() {
+        console.log('Hello TS');
+    }
+}
+
+/// Je suis à l'extérieur de la classe
+new Main();
