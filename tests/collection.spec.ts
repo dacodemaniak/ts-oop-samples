c98ab15840507a2f8fe4a10e448ef0815478ca88
dacodemaniak <jean-luc.aubert@aelion.fr>
2022-11-03 09:00:52 +0100

    Spec for collection validation

--- tests/collection.spec.ts
+++ tests/collection.spec.ts
@@ -1,3 +1,4 @@
+import { StagiaireModel } from "../src/models/stagiaire-model";
 import { Repository } from "../src/repositories/repository";
 import { StagiaireRepository } from "../src/repositories/stagiaire-repository"
 
@@ -5,5 +6,20 @@ describe(`StagiaireCollection`, () => {
     it(`Should instanciate a Collection Object`, () => {
         const stagiaireRepository: StagiaireRepository = new StagiaireRepository();
         expect(stagiaireRepository).toBeInstanceOf(Repository);
+    });
+
+    it (`getSize() should return one after using add method`, () => {
+        const repository: StagiaireRepository = new StagiaireRepository();
+        const stagiaire = new StagiaireModel();
+        repository.add(stagiaire);
+        expect(repository.getSize()).toBe(1);
+    })
+
+    it(`getSize() should return one even if add called twice with same Item`, () => {
+        const repository: StagiaireRepository = new StagiaireRepository();
+        const stagiaire = new StagiaireModel();
+        repository.add(stagiaire);
+        repository.add(stagiaire);
+        expect(repository.getSize()).toBe(1);
     })
 })
